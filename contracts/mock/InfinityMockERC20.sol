e7d643d23fdf8bce180b51c63f89d37426893ef0
Adi Kancherla <findadit@gmail.com>
2021-12-19 13:32:44 -0600

    c

--- contracts/mock/InfinityMockERC20.sol
+++ contracts/mock/InfinityMockERC20.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.4;
 import {SafeMath} from '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract MockERC20 is ERC20('Mock', 'MOCK') {
+contract InfinityMockERC20 is ERC20('InfinityMock', 'INFTMOCK') {
   constructor() {
     _mint(msg.sender, 100_000_000 * (10**decimals()));
   }
--- tasks/default.ts
+++ tasks/default.ts
@@ -35,3 +35,37 @@ task('deployInfinityFactory', 'Deploy')
       });
     }
   });
+
+  task('deployInfinityMockERC20', 'Deploy')
+    .addFlag('verify', 'verify contracts on etherscan')
+    .setAction(async (args, { ethers, run, network }) => {
+      // log config
+      console.log('Network');
+      console.log('  ', network.name);
+      console.log('Task Args');
+      console.log(args);
+
+      // compile
+      await run('compile');
+      // get signer
+      const signer = (await ethers.getSigners())[0];
+      console.log('Signer');
+      console.log('  at', signer.address);
+      console.log('  ETH', formatEther(await signer.getBalance()));
+
+      const infinityMockERC20 = await deployContract(
+        'InfinityMockERC20',
+        await ethers.getContractFactory('InfinityMockERC20'),
+        signer
+      );
+
+      // verify source
+      if (args.verify) {
+        console.log('Verifying source on etherscan');
+        await infinityMockERC20.deployTransaction.wait(5);
+        await run('verify:verify', {
+          address: infinityMockERC20.address,
+          contract: 'contracts/mock/InfinityMockERC20.sol:InfinityMockERC20'
+        });
+      }
+    });
